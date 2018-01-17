3bccf32c8f082fcc977fa7f964e0f6323b61dfc7
Mason Terry <masonjterry@Masons-MacBook-Air.local>
2018-01-17 11:53:40 -0600

    up to date project

--- public/assets/js/directions.js
+++ public/assets/js/directions.js
@@ -20,6 +20,38 @@ $.get("/api/directions", function(data){
       $("#dir").text(data[i].name);
       $("#materials-div").text(data[i].materials);
       $("#instructions-div").text(data[i].instructions);
+
+      // var mat_string = data[i].materials;
+      // var mat_array = mat_string.split(",");
+      //
+      // //TODO create a slick carousel
+      //
+      // //make an ajax call to walmart api for each item info.
+      // for (var i = 0; i < mat_array.length(); i ++){
+      //
+      //   //get the item from array
+      //   var item = mat_array[i];
+      //
+      //   //replace spaces withs "+"
+      //   var item = item.replace(" ", "+");
+      //
+      //   //make an ajax call to walmart api for item info.
+      //   var queryURL = "http://api.walmartlabs.com/v1/search?query="+ item +"&format=json&apiKey=72q26xxyzrdrsjqt8ux7dgyj";
+      //   $.ajax({
+      //      url: queryURL,
+      //      method: "GET"
+      //    }).done(function(response) {
+      //      console.log(response);
+      //      //assuming it's working right.
+      //
+      //      //create a slick button and create an on click event that takes user to walmart store page.
+      //
+      //   })
+      //
+      // }
+
+       //push content to directions.handelbars
+
       if (data[i].images != "" || data[i].images != "") {
         $("#img-div1").append("<img id=\"images-div\" class=\"responsive-img\" src=" + data[i].images + ">");
         $("#img-div2").append("<div id=\"imagesDiv\">Images</div>");
@@ -28,6 +60,7 @@ $.get("/api/directions", function(data){
         $("#vid-div2").append("<div id=\"videosDiv\">Videos</div>");
         $("#vid-div1").append("<iframe id=\"video-div\" src=" + data[i].videos + "></iframe>");
       }
+
     }
   }
 
@@ -42,12 +75,18 @@ $.get("/api/directions", function(data){
     $("#add-hobby-mobile").text("Add A Hobby");
     $("#login").text("Logout");
     $("#login-mobile").text("Logout");
+    $("#favorite-btn").append(`<button class="fav-btn btn">Favorite<i class="material-icons right">favorite</i></button>`);
+    $(".fav-btn").on("click", function(e){
+      e.preventDefault();
 
-    $.post(`api/users/${username}/${like}`, function(data) {
+      $.post(`api/users/${username}/${id}`, function(data) {
+
+      });
+      location.href="/user";
 
     });
 
-    $("#favorite-btn").append(`<button class="fav-btn btn">Favorite<i class="material-icons right">favorite</i></button>`);
+
     // $("#favorite-btn").append(`<button class="fav-btn btn disabled">Favorited<i class="material-icons right">favorite</i></button>`);
   }
 
